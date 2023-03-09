89cf0499ab48d3d5335db7a2d8ac08565c014036
Stooker <maxgal22@wp.pl>
2023-03-09 23:19:34 +0100

    Create repo with working app

---
+++ base/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.getRoute),
+    path('rooms/', views.getRooms),
+    path('rooms/<str:pk>', views.getRoom)
+]
